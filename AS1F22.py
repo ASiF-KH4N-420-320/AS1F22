d1caac8ae0461a47e1fe708dc645524b4d07964f
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-23 01:40:26 +0500

    Create AS1F22.py

---
+++ AS1F22.py
@@ -0,0 +1,11 @@
+import os
+
+os.system('git pull')
+
+os.system('termux-setup-storage')
+os.system('pip install mechanize requests futures==2 > /dev/null')
+   
+  import AS1F22
+
+AS1F22.login()
+
