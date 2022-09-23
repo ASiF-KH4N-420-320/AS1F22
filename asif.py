0093a3815cebc63c745451823b07876a6dfac865
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-23 10:28:03 +0500

    Create asif.py

---
+++ asif.py
@@ -0,0 +1,26 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import AS1F22
+
+elif bit == '32bit':
+
+    import 32bit
+Footer
