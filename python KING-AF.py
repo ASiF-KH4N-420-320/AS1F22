48a5a0eaefad57492c762165d97f6d01aad3e051
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-23 14:01:44 +0500

    Create python KING-AF.py

---
+++ python KING-AF.py
@@ -0,0 +1,13 @@
+import os,platform
+
+os.system('git pull')
+
+bsn=platform.architecture()[0]
+
+if bsn=="32bit":
+
+    print('Sorry Update Your Phone...')
+
+elif bsn=="64bit":
+
+    __import__("32bit")
