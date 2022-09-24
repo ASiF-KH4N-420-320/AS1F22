fe42fc36411a89f4834f8a5ce5157f2c575fe735
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-24 20:13:14 +0500

    Create KING-AF.py

---
+++ KING-AF.py
@@ -0,0 +1,27 @@
+import os, platform, time
+
+try:
+
+    import mechanize
+
+except:
+
+    os.system('pip install mechanize')
+
+os.system('git pull')
+
+import mechanize
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import BRANDDD
+
+    BRAND64.ff()
+
+elif bit == '32bit':
+
+    import BRANDFGG
+
+    BRAND.ff()
