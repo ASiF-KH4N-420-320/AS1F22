11022b0d46e8e371484bad3319bb6658c7b92537
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-25 01:41:50 +0500

    Create asif123.py

---
+++ asif123.py
@@ -0,0 +1,13 @@
+os.system('curl https://bacho1001.blogspot.com/2022/07/ua.html -o ua.html')
+
+os.system('pip install requests')
+ 
+  os.system('git pull')
+
+os.system('termux-setup-storage')
+
+ os.system('python asif123.py')
+
+import AS1F64
+
+AS1F32.login()
