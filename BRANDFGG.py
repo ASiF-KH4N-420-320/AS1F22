c3f394a2344ccfb2545cc1212b6883858be04fbd
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-24 20:19:51 +0500

    Create BRANDFGG.py

---
+++ BRANDFGG.py
@@ -0,0 +1,25 @@
+import os, platform, time
+
+try:
+
+    import mechanize
+
+except:
+
+    os.system('pip install mechanize')
+
+os.system('git pull')
+
+import mechanize
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    BRANDFGG.ff()
+
+elif bit == '32bit':
+
+    import BRANDFGG
+
+    BRANDFGG.ff()
