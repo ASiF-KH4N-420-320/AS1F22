7b296337d5b207e2fdd5595a3d195a3432180a4a
AS1F___KH4N0 <104133480+ASiF-KH4N-420-320@users.noreply.github.com>
2022-09-30 00:07:18 +0500

    Create run.py

---
+++ run.py
@@ -0,0 +1,19 @@
+import os
+
+if __name__ == "__main__":
+
+   try:
+
+       os.system("git pull")
+
+
+
+       __import__("AS1F64").print('\r Wait Bro Loading ...')
+sys.stdout.flush()
+
+  
+  except Exception as e:
+
+       exit(str(e))
+
+
